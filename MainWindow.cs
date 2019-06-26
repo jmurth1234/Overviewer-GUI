bcba3715bb77efc357b3594e9947cc6a10b9fe8e
Ryan <supermarioryan@gmail.com>
2019-06-26 20:42:22 +0100

    Ensure config is outside the store sandbox

--- MainWindow.cs
+++ MainWindow.cs
@@ -333,7 +333,7 @@ namespace OverviewerGUI
                 Worlds = new List<World> { world }
             };
 
-            var file = GetFile("config.generated.py");
+            var file = GetOverviewer("config.generated.py");
             File.WriteAllText(file, config.generateConfig());
 
             configRender(file);
